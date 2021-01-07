146124aa6a01a4667cbe24c9fb693e512a0030ef
Larissa Andrade <44838761+lariandrade@users.noreply.github.com>
2021-01-07 15:22:16 -0300

    resolvido bug

---
+++ javascript.js
@@ -0,0 +1,10 @@
+const projeto = document.querySelector(".projetos")
+const linkedin = document.querySelector(".linkedin")
+
+projeto.addEventListener("click", function() {
+  window.open('paginas/projetos.html')
+  }); 
+
+linkedin.addEventListener("click", function() {
+    window.open('https://www.linkedin.com/in/larissa-sandrade/', '_blank')
+  }); 
